f19ebb814550081ece272b56261b8d91a7195a5e
Pisnut <rayner.freddie@btinternet.com>
2025-06-18 17:11:15 +0100

    Create preload.js, setup ipc

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
-const os = require("os");
-const pty = require("node-pty");
 
 function createWindow() {
 	const win = new BrowserWindow({
@@ -11,7 +9,7 @@ function createWindow() {
 		webPreferences: {
 			preload: path.join(__dirname, "preload.js"),
 			nodeIntegration: true,
-			contextIsolation: false,
+			contextIsolation: true,
 		},
 	});
 
@@ -20,26 +18,9 @@ function createWindow() {
 
 app.whenReady().then(createWindow);
 
-ptyProcess = pty.spawn(os.platform() === "win32" ? "cmd.exe" : "bash", [], {
-	name: "xterm-color",
-	cols: 80,
-	rows: 30,
-	cwd: process.env.HOME,
-	env: process.env,
-});
-
-// Send shell output to renderer
-ptyProcess.onData((data) => {
-	win.webContents.send("terminal-incData", data);
-});
-
 // Receive input from renderer
-ipcMain.on("terminal-toShell", (event, input) => {
-	ptyProcess.write(input);
-});
-
-ipcMain.on("terminal-resize", (event, { cols, rows }) => {
-	ptyProcess.resize(cols, rows);
+ipcMain.on("input", (event, input) => {
+	console.log("Received input:", input);
 });
 
 app.on("window-all-closed", () => {
--- preload.js
+++ preload.js
@@ -1,8 +1,5 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electronAPI", {
-	sendInput: (data) => ipcRenderer.send("terminal-toShell", data),
-	onTerminalData: (callback) =>
-		ipcRenderer.on("terminal-incData", (event, data) => callback(data)),
-	resize: (size) => ipcRenderer.send("terminal-resize", size),
+	sendInput: (data) => ipcRenderer.send("input", data),
 });
